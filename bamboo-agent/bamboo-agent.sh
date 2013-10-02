6971c8adbf8b72fd330bc7557525542c76cdf513
Hannes Wüthrich <hw@5px.ch>
2013-10-02 16:08:13 +0200

    bamboo-agent: Use official FROM tag, rename bamboo-agent script

--- bamboo-agent/bamboo-agent.sh
+++ bamboo-agent/bamboo-agent.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e # Exit on errors
+
 echo "-> Starting Bamboo Agent ..."
 echo "   - BAMBOO_HOME:   $BAMBOO_HOME"
 echo "   - BAMBOO_SERVER: $BAMBOO_SERVER"
@@ -12,7 +14,7 @@ if [ -f $BAMBOO_INSTALLER ]; then
 else
   BAMBOO_INSTALLER_URL=$BAMBOO_SERVER/agentServer/agentInstaller
   echo "-> Downloading installer from $BAMBOO_INSTALLER_URL ..."
-  wget $BAMBOO_INSTALLER_URL -O $BAMBOO_INSTALLER
+  wget --progress=dot:mega $BAMBOO_INSTALLER_URL -O $BAMBOO_INSTALLER
 fi
 
 echo "-> Running Bamboo Installer ..."
