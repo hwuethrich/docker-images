494f76795538f24d54286297768fe8375b9bcf36
Hannes Wüthrich <hw@5px.ch>
2013-10-03 17:27:23 +0200

    Remove intermediate containers after build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e # Exit on errors
 for image in base java bamboo-server bamboo-agent bamboo-ruby
 do
     echo "=> Building $image ..."
-    docker build -t hwuethrich/$image $image > /dev/null
+    docker build -rm -t hwuethrich/$image $image > /dev/null
 done
