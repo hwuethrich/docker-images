a581e4aa3b44079dbf50670612562afb50dfa020
Hannes Wüthrich <hw@5px.ch>
2013-10-14 13:24:09 +0200

    Use non-system user for bamboo with group bamboo

--- bamboo-agent/bamboo-agent.sh
+++ bamboo-agent/bamboo-agent.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # Fix permissions
-chown -R $BAMBOO_USER:nogroup $BAMBOO_HOME
+chown -R $BAMBOO_USER:$BAMBOO_USER $BAMBOO_HOME
 
 echo "-> Running Bamboo Installer ..."
 su - $BAMBOO_USER -c "java -Dbamboo.home=$BAMBOO_HOME -jar $BAMBOO_INSTALLER $BAMBOO_SERVER/agentServer/" &
