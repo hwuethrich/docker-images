fa511e987315453876977ccf3748eb1dc4551cc5
Hannes Wüthrich <hw@5px.ch>
2013-10-14 13:25:18 +0200

    Install rbenv in BAMBOO_HOME instead of globally

--- bamboo-ruby/install/rbenv.sh
+++ bamboo-ruby/install/rbenv.sh
@@ -3,17 +3,17 @@
 set -e # Exit on errors
 
 # Essentials
-apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
-apt-get clean
+apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config 
+
+# Install rbenv in BAMBOO_HOME
+export RBENV_ROOT=$BAMBOO_HOME/.rbenv
 
 # Install rbenv
 git clone https://github.com/sstephenson/rbenv.git $RBENV_ROOT
 
 # Enable rbenv for shells
-echo "export RBENV_ROOT=$RBENV_ROOT"                       >> /etc/profile.d/rbenv.sh
-echo 'export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH' >> /etc/profile.d/rbenv.sh
-echo 'eval "$($RBENV_ROOT/bin/rbenv init -)"'              >> /etc/profile.d/rbenv.sh
-chmod +x /etc/profile.d/rbenv.sh
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $BAMBOO_HOME/.bashrc
+echo 'eval "$(rbenv init -)"' >> $BAMBOO_HOME/.bashrc
 
 # Install ruby-build & rbenv-aliases
 mkdir -p $RBENV_ROOT/plugins
@@ -24,3 +24,6 @@ git clone git://github.com/tpope/rbenv-aliases.git      $RBENV_ROOT/plugins/rben
 echo "gem: --no-ri --no-rdoc"     >> $BAMBOO_HOME/.gemrc
 echo "install: --no-ri --no-rdoc" >> $BAMBOO_HOME/.gemrc
 echo "update: --no-ri --no-rdoc"  >> $BAMBOO_HOME/.gemrc
+
+# Fix permissions
+chown -R $BAMBOO_USER:$BAMBOO_USER $BAMBOO_HOME
