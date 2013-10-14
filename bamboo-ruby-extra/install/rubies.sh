0d021bd8eb20cb2f9b29132fe700e3ed54cfe2e3
Hannes Wüthrich <hw@5px.ch>
2013-10-14 13:28:26 +0200

    Install rubies in /.rbenv

--- bamboo-ruby-extra/install/rubies.sh
+++ bamboo-ruby-extra/install/rubies.sh
@@ -4,14 +4,12 @@
 for version in 1.9.3-p448 2.0.0-p247 2.1.0-preview1
 do
   rbenv install $version
-  RBENV_VERSION=$version gem install bundler --no-rdoc --no-ri
+  RBENV_VERSION=$version gem update --system
+  RBENV_VERSION=$version gem install bundler --pre --no-rdoc --no-ri
 done
 
 # Make latest 2.0.0 default
 rbenv global 2.0.0
-
-# Configure bundler to use global directory for gems to save disk space
-su - $BAMBOO_USER -c "bundle config path $BAMBOO_HOME/.bundle"
 
 # Add Bamboo agent commands
 rbenv rehash
