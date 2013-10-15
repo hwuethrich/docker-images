890f6f50d41d78d51921f33bfba6a8f7d7d6148f
Hannes Wüthrich <hw@5px.ch>
2013-10-16 00:10:32 +0200

    Be verbose when pushing

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,5 +5,5 @@ set -e # Exit on errors
 for image in base java bamboo-server bamboo-agent bamboo-ruby bamboo-ruby-extra
 do
     echo "=> Pushing $image ..."
-    docker push hwuethrich/$image > /dev/null
+    docker push hwuethrich/$image
 done
