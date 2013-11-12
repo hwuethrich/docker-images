43300313201b4bfe1807c513d7255cacfd2f9451
Hannes Wüthrich <hw@5px.ch>
2013-11-12 15:16:25 +0100

    Refactoring cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
 
+function ask {
+	read -p "$1 " -n 1 -r
+  [[ $REPLY =~ ^[Yy]$ ]] || exit
+  echo -e "\n"
+}
+
+function untagged_images {
+	sudo docker images | grep --color=never '<none>'
+}
+
+function stopped_containers {
+	sudo docker ps -a | grep --color=never 'Exit'
+}
+
 # CONTAINERS
+count=$(stopped_containers | wc -l)
+if [[ $count -gt 0 ]]; then
+	sudo docker ps | head -1
+  stopped_containers
 
-echo "Removing stopped containers:"
-docker ps -a | grep --color=never 'Exit'
-read -p "Are you sure? " -n 1 -r
-
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
-
-docker ps -a | grep 'Exit' | awk '{print $1}' | xargs docker rm
-
+	ask "Remove $count stopped containers?" || exit
+  echo "Removing containers ..."
+  stopped_containers | awk '{print $1}' | sudo xargs docker rm
+else
+	echo "No stopped containers found"
+fi
 
 # IMAGES
+count=$(untagged_images | wc -l)
+if [[ $count -gt 0 ]]; then
+	sudo docker images | head -1
+  untagged_images
 
-echo "Removing untagged images:"
-docker images | grep --color=never '<none>'
-read -p "Are you sure? " -n 1 -r
-
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
-
-docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-
+	ask "Remove $count untagged images?" || exit
+	echo "Removing images ..."
+	untagged_images | awk '{print $3}' | sudo xargs docker rmi
+else
+	echo "No untagged images found"
+fi
