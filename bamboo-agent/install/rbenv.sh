5efc559c8db8a754d4ca557b998bc7325be315fc
Hannes Wüthrich <hw@5px.ch>
2013-11-12 14:11:11 +0100

    Use sstephenson/rbenv-default-gems to install default gems

--- bamboo-agent/install/rbenv.sh
+++ bamboo-agent/install/rbenv.sh
@@ -3,7 +3,7 @@
 set -e # Exit on errors
 
 # Essentials
-eatmydata apt-get -y install \
+eatmydata -- apt-get -yq install \
 	build-essential openssl libreadline6 libreadline6-dev curl wget git \
 	zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
 	libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake \
@@ -30,13 +30,21 @@ eval "$(rbenv init -)"
 
 # Install ruby-build & rbenv-aliases
 mkdir -p $RBENV_ROOT/plugins
-git clone https://github.com/sstephenson/ruby-build.git        $RBENV_ROOT/plugins/ruby-build
-git clone https://github.com/tpope/rbenv-aliases.git           $RBENV_ROOT/plugins/rbenv-aliases
-git clone https://github.com/sstephenson/rbenv-gem-rehash.git  $RBENV_ROOT/plugins/rbenv-gem-rehash
+git clone https://github.com/sstephenson/ruby-build.git         $RBENV_ROOT/plugins/ruby-build
+git clone https://github.com/tpope/rbenv-aliases.git            $RBENV_ROOT/plugins/rbenv-aliases
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git   $RBENV_ROOT/plugins/rbenv-gem-rehash
+git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_ROOT/plugins/rbenv-default-gems
+
+# Default gems
+tee $RBENV_ROOT/default-gems <<-EOF
+	bundler --pre
+EOF
 
 # Disable rdoc/ri
-echo "install: --no-document" >> $BAMBOO_HOME/.gemrc
-echo "update: --no-document"  >> $BAMBOO_HOME/.gemrc
+tee $BAMBOO_HOME/.gemrc <<-EOF
+	install: --no-document --no-ri --no-rdoc
+	update:  --no-document --no-ri --no-rdoc
+EOF
 
 # Fix permissions
 chown -R $BAMBOO_USER:$BAMBOO_USER $RBENV_ROOT
--- bamboo-agent/install/rubies.sh
+++ bamboo-agent/install/rubies.sh
@@ -5,18 +5,18 @@ for version in 1.9.3-p448 2.0.0-p247 2.1.0-preview1
 do
   CONFIGURE_OPTS="--disable-install-rdoc" MAKE_OPTS="-j8" eatmydata rbenv install $version
   RBENV_VERSION=$version gem update --system
-  RBENV_VERSION=$version gem install bundler --pre --no-document
 done
 
 # Make latest 2.0.0 default
 rbenv global 2.0.0
 
+# Add shims as bamboo build commands
 for shim in $RBENV_ROOT/shims/*; do
   shim=$(basename $shim) # Strip path
   bamboo-capability-command $shim
 done
 
-# Add capabilities
+# Add ruby versions as capabilities
 for version in $RBENV_ROOT/versions/*; do
   version=$(basename $version) # Strip path
   bamboo-capability "Ruby $version" "`RBENV_VERSION=$version ruby -v`"
